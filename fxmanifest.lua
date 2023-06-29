0096f0aebe3865fbd4dbffb99e7982b3421c26ad
Mahan Moulaei <75180223+mahanmoulaei@users.noreply.github.com>
2023-06-28 23:14:22 -0400

    inital commit

---
+++ fxmanifest.lua
@@ -0,0 +1,27 @@
+fx_version "cerulean"
+use_experimental_fxv2_oal "yes"
+lua54 "yes"
+game "gta5"
+
+name "esx_garages"
+version "0.0.0"
+description "ESX-Overextended Garages"
+
+dependencies {
+    "es_extended",
+    "ox_target"
+}
+
+shared_scripts {
+    "@es_extended/imports.lua",
+    "@ox_lib/init.lua",
+    "shared/*.lua",
+}
+
+server_scripts {
+    "server/*.lua"
+}
+
+client_scripts {
+    "client/*.lua",
+}
