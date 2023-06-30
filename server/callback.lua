76f2b71ca8bd3184f2af10a9e55515fe78b809a9
Mahan Moulaei <75180223+mahanmoulaei@users.noreply.github.com>
2023-06-30 02:50:21 -0400

    refactor(server): better vehicle info in garage menu

--- server/callback.lua
+++ server/callback.lua
@@ -9,7 +9,7 @@ lib.callback.register("esx_garages:getOwnedVehicles", function(source, garageKey
     local currentGarage = Config.Garages[garageKey]
     local contextOptions = {}
 
-    local dbResults = MySQL.rawExecute.await("SELECT `id`, `plate`, `vehicle`, `model`, `stored`, `garage` FROM `owned_vehicles` WHERE `owner` = ? AND `type` = ? AND `job` IS NULL", { xPlayer.getIdentifier(), currentGarage.Type })
+    local dbResults = MySQL.rawExecute.await("SELECT ov.`id`, ov.`plate`, ov.`vehicle`, ov.`model`, ov.`stored`, ov.`garage` FROM `owned_vehicles` AS ov LEFT JOIN `impounded_vehicles` AS iv ON ov.`id` = iv.`id` WHERE ov.`owner` = ? AND ov.`type` = ? AND ov.`job` IS NULL", { xPlayer.getIdentifier(), currentGarage.Type })
 
     for i = 1, #dbResults do
         local dbResult = dbResults[i]
@@ -24,7 +24,7 @@ lib.callback.register("esx_garages:getOwnedVehicles", function(source, garageKey
             end
         end
 
-        if not dbResult.model then print(("[^3WARNING^7] Vehicle hash (^1%s^7) is invalid \nEnsure vehicle exists in ^2'@es_extended/files/vehicles.json'^7"):format(dbResult.vehicle?.model)) goto skipLoop end
+        if not dbResult.model then print(("[^3WARNING^7] Vehicle hash (^1%s^7) for ID (^5%s^7) is invalid \nEnsure vehicle exists in ^2'@es_extended/files/vehicles.json'^7"):format(dbResult.vehicle?.model, dbResult.id)) goto skipLoop end
 
         vehiclesCount += 1
         vehicles[vehiclesCount] = {
@@ -38,15 +38,44 @@ lib.callback.register("esx_garages:getOwnedVehicles", function(source, garageKey
         local modelData = ESX.GetVehicleData(dbResult.model)
         local vehicleName = ("%s %s"):format(modelData.make, modelData.name)
 
+        local contextDescription = ("Plate: %s"):format(dbResult.plate)
+        local contextMetadata = {
+            { label = "Status", value = vehicles[vehiclesCount].stored and ("Stored in %s"):format(dbResult.garage == garageKey and "Here" or Config.Garages[dbResult.garage]?.Label) or "Out" }
+        }
+
+        if dbResult.vehicle.plate ~= dbResult.plate then
+            contextDescription = ("%s - %s"):format(contextDescription, ("Fake Plate: %s"):format(dbResult.vehicle.plate))
+        end
+
+        if vehicles[vehiclesCount].stored and dbResult.vehicle then
+            if dbResult.vehicle.fuelLevel then
+                local fuelLevel = dbResult.vehicle.fuelLevel
+                contextMetadata[#contextMetadata + 1] = { label = "Fuel", value = ("%%%s"):format(fuelLevel), progress = fuelLevel }
+            end
+
+            if dbResult.vehicle.bodyHealth then
+                local bodyHealth = dbResult.vehicle.bodyHealth / 10
+                contextMetadata[#contextMetadata + 1] = { label = "Body Health", value = ("%%%s"):format(bodyHealth), progress = bodyHealth }
+            end
+
+            if dbResult.vehicle.engineHealth then
+                local engineHealth = dbResult.vehicle.engineHealth / 10
+                contextMetadata[#contextMetadata + 1] = { label = "Engine Health", value = ("%%%s"):format(engineHealth), progress = engineHealth }
+            end
+        end
+
+        if dbResult.impounded_at then
+            contextDescription = "Impounded"
+        end
+
         contextOptions[vehiclesCount] = {
             title = vehicleName,
+            description = contextDescription,
             arrow = vehicles[vehiclesCount].stored,
+            disabled = dbResult.impounded_at ~= nil,
             event = vehicles[vehiclesCount].stored and "esx_garages:openVehicleMenu",
             args = { vehicleName = vehicleName, vehicleId = dbResult.id, plate = dbResult.plate, storedGarage = dbResult.garage, garageKey = garageKey },
-            metadata = {
-                ["Plate"] = dbResult.vehicle?.plate or dbResult.plate,
-                ["Status"] = vehicles[vehiclesCount].stored and ("Stored in %s"):format(dbResult.garage == garageKey and "Here" or Config.Garages[dbResult.garage]?.Label) or "Out"
-            }
+            metadata = contextMetadata
         }
 
         ::skipLoop::
