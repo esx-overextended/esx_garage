eb09aadf8730621fbc109e6646d2ee89926d24af
Mahan Moulaei <75180223+mahanmoulaei@users.noreply.github.com>
2025-04-30 04:07:43 -0400

    feat(shared): add exports to get all garages config

--- shared/shared.lua
+++ shared/shared.lua
@@ -1,3 +1,7 @@
 Shared = {}
 
 Shared.AvailableSpawnPointIndicator = "client" -- indicates whether the server or client should be responsible for locating closest available spawn point
+
+exports("GetGarages", function()
+    return Config.Garages
+end)
